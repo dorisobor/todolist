3457ce9c90f52a12cfafb929b5fca84194b38fc4
dorisobor <troyamber@live.com>
2017-11-17 12:32:28 +0100

    delete a task

--- delete-task.php
+++ delete-task.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require "database.php";
 
@@ -22,29 +21,4 @@ $statement= $pdo->prepare("
 }
 header('Location: index.php')
 
-=======
-<?php
-require "database.php";
-
-
-// function to delete the task
-if(isset ($_GET['task'])){
-  $task = $_GET['task'];
-
-
-// delete task
-$statement= $pdo->prepare("
-			DELETE FROM todos 
-            WHERE id = :id
-			");
-		
- $statement ->execute([
-'id' => $task
- ]);
- 
-   
-}
-header('Location: index.php')
-
->>>>>>> 04533909290c4708ea056e4263ec8ce6d9de594a
 ?>
