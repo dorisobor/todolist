a6d003ac62aa3835a72a2f51cfc3796459b8c3ae
dorisobor <troyamber@live.com>
2017-11-17 12:45:28 +0100

    marks the task as done

--- marked-done.php
+++ marked-done.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require "database.php";
 
@@ -22,29 +21,4 @@ if(isset($_GET['task'])){
 
 header('Location: index.php')
 
-=======
-<?php
-require "database.php";
-
-// function to mark the task done
-if(isset($_GET['task'])){
-	$task = $_GET['task'];
-
-        //Prepare statement to update the task to done
-  
-				$statement = $pdo->prepare("
-					UPDATE todos SET completed = 1
-					WHERE id = :task
-					");
-
-				$statement ->execute([
-					'task' => $task
-					
-					]);
-		
-	}
-
-header('Location: index.php')
-
->>>>>>> 04533909290c4708ea056e4263ec8ce6d9de594a
 ?>
